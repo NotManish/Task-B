b54f52f80942df15d579df8f2866df186fb61aa0
NotManish <manishshrestha051@gmail.com>
2025-01-13 21:52:48 +0545

    Initial setup to the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-red-500 text text-5xl">
+      Tailwind CSS is working!
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Include this if using index.html
+    './src/**/*.{js,jsx,ts,tsx}', // Include all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
